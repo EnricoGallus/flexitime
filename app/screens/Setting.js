69d0850f925e565708f8a04fee554cf3cdc9bba3
enrico gallus <enrico.gallus@gmail.com>
2017-04-11 12:43:49 +0900

    enhanced settings and added editable items

--- app/screens/Setting.js
+++ app/screens/Setting.js
@@ -13,76 +13,79 @@ import SettingsList from 'react-native-settings-list';
 class Setting extends Component {
    constructor(){
      super();
-     this.onValueChange = this.onValueChange.bind(this);
      this.state = {
-		 	switchValue: false,
-			weeklyHours: 40,
-			daysPerWeek: 5,
-			hoursPerDay: 8
-		 };
-   }    
-   render() {
-	   return (
-			<View style={{backgroundColor:'gray',flex:1}}>
-				<View style={{flex:1, marginTop:50}}>
-					<SettingsList>
-						<SettingsList.Header headerText='Einstellung' headerStyle={styles.header}/>
-						<SettingsList.Item
-							hasNavArrow={false}
-							hasSwitch={false}
-							title='Wochenarbeitszeit'
-							isEditable={true}
-							placeholder={this.state.weeklyHours.toString()}
-							onPress={() => this.setState({weeklyHours: this.state.weeklyHours}) } />
-						<SettingsList.Item
-							hasNavArrow={false}
-							isEditable={true}
-							hasSwitch={false}
-							title='Tage/Woche'
-							placeholder={this.state.daysPerWeek.toString()}
-							onPress={() => this.setState({daysPerWeek: this.state.daysPerWeek}) }  />
-						<SettingsList.Item
-							hasNavArrow={false}
-							hasSwitch={false}
-							title='Stunde/Tag'
-							isEditable={true}
-							placeholder={this.state.hoursPerDay.toString()}
-							onPress={() => this.setState({hoursPerDay: this.state.hoursPerDay}) }  />
-						<SettingsList.Header headerText='Enhanced' headerStyle={styles.header}/>
-						<SettingsList.Item
-							hasNavArrow={false}
-							switchState={this.state.switchValue}
-							switchOnValueChange={this.onValueChange}
-							hasSwitch={true}
-							title='Geofencing' />
-						<SettingsList.Item
-							hasNavArrow={true}
-							switchState={this.state.switchValue}
-							switchOnValueChange={this.onValueChange}
-							hasSwitch={false}
-							title='????Übernahme/Stunden????' />
-					</SettingsList>
-				</View>
-			</View>
-	   );
+            isGeofencing: false,
+            weeklyHours: 40,
+            daysPerWeek: 5,
+            hoursPerDay: 8
+         };
    }
 
-   onValueChange(value){
-     this.setState({switchValue: value});
+   render() {
+       return (
+            <View style={styles.page}>
+                <View style={styles.container}>
+                    <SettingsList>
+                        <SettingsList.Header headerText='Einstellung' headerStyle={styles.header}/>
+                        <SettingsList.Item
+                            id="weeklyHour"
+                            hasNavArrow={false}
+                            hasSwitch={false}
+                            title='Wochenarbeitszeit'
+                            isEditable={true}
+                            value={this.state.weeklyHours.toString()}
+                            onTextChange={(text) => this.setState({weeklyHours: text})} 
+                            />
+                        <SettingsList.Item
+                            id="daysPerWeek"
+                            hasNavArrow={false}
+                            isEditable={true}
+                            hasSwitch={false}
+                            title='Tage/Woche'
+                            value={this.state.daysPerWeek.toString()}
+                            onTextChange={(text) => this.setState({daysPerWeek: text})} 
+                            />
+                        <SettingsList.Item
+                            id="hoursPerDay"
+                            hasNavArrow={false}
+                            hasSwitch={false}
+                            title='Stunde/Tag'
+                            isEditable={true}
+                            value={this.state.hoursPerDay.toString()}
+                            onPress={(text) => this.setState({hoursPerDay: text})} />
+                        <SettingsList.Header headerText='Enhanced' headerStyle={styles.header}/>
+                        <SettingsList.Item
+                            hasNavArrow={false}
+                            switchState={this.state.isGeofencing}
+                            switchOnValueChange={(value) => this.setState({isGeofencing: value})}
+                            hasSwitch={true}
+                            title='Geofencing' />
+                        <SettingsList.Item
+                            hasNavArrow={true}
+                            switchState={this.state.switchValue}
+                            switchOnValueChange={this.onValueChange}
+                            hasSwitch={false}
+                            title='????Übernahme/Stunden????' />
+                    </SettingsList>
+                </View>
+            </View>
+       );
    }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1, 
-		alignItems: 'center', 
-		justifyContent: 'center', 
-		backgroundColor: '#f2ae72'
-	},
-	header: {
-		color: 'white', 
-		marginTop: 50
-	}
+    page: {
+        flex:1,
+        backgroundColor:'gray'
+    },
+    container: {
+        flex: 1,
+        marginTop: 50
+    },
+    header: {
+        color: 'white',
+        marginTop: 50
+    }
 })
 
 export default Setting;
